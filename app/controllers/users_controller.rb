e8a1c7a9183cb1037b11935705e346cf8146c772
izeal <ivanz_89@mail.ru>
2018-04-17 09:00:37 +0300

    try to make edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Вы успешно зарегистрировались"
+      redirect_to users_path
+    else
+      flash[:danger] = "Корректно заполните все поля"
+      render 'new'
+    end
   end
 
   def show
@@ -14,11 +23,27 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.new
   end
 
   def update
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user), notice: "Данные успешно обновлены"
+    else
+      render edit_user(@user), notice: "Мы не смогли обновить ваш профиль по следующим причинам:"
+    end
   end
 
   def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:success] = "Пользователь успешно удален"
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :name, :login, :email, :password, :password_confirmation
+    )
   end
 end
