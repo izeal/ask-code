1126be0ee633c0474159bdd4278698fd9aab2f6a
izeal <ivanz_89@mail.ru>
2018-05-09 14:48:49 +0300

    add partials

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create(
     avatar_url: 'https://avatars3.githubusercontent.com/u/31633474?s=460&v=4'
 )
 
-100.times do |n|
+20.times do |n|
   name = Faker::Name.name
   login = Faker::LordOfTheRings.character.gsub(" ", "_")
   avatar_url = Faker::Avatar.image
@@ -21,20 +21,20 @@ User.create(
   )
 end
 
-100.times do |n|
-  name = Faker::Name.name
-  login = Faker::HarryPotter.character.gsub(" ", "_")
-  avatar_url = Faker::Avatar.image
-  header_background_color = Faker::Color.hex_color
-  User.create(
-    name: name,
-    login: "#{login}_#{n}",
-    avatar_url: avatar_url,
-    header_background_color: header_background_color,
-    email: "#{login}#{n}@bar.baz",
-    password: 'foobar'
-  )
-end
+# 100.times do |n|
+#   name = Faker::Name.name
+#   login = Faker::HarryPotter.character.gsub(" ", "_")
+#   avatar_url = Faker::Avatar.image
+#   header_background_color = Faker::Color.hex_color
+#   User.create(
+#     name: name,
+#     login: "#{login}_#{n}",
+#     avatar_url: avatar_url,
+#     header_background_color: header_background_color,
+#     email: "#{login}#{n}@bar.baz",
+#     password: 'foobar'
+#   )
+# end
 
 users = User.all
 users.each do |user|
