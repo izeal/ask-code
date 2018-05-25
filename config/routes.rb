5d41b7240aa01f84ed6b369ebf96c085349a33fb
izeal <ivanz_89@mail.ru>
2018-05-25 19:38:18 +0300

    fix sessions to session & delete logout_path and other

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,11 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users
-  resource :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
   resources :posts do
     resources :comments
   end
   resources :hashtags, only: [:show] do
     get "posts"
   end
-
-  get 'signup', to: 'users#new'
-  get 'login', to: 'sessions#new'
-  delete 'logout', to: 'sessions#destroy'
 end
